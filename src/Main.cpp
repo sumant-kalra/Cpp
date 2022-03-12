70ceb9a08703ab83886891a57129a4640596e78f
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-03-12 22:32:01 +0530

    chg: dev: Improved-1

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <bitset>
+#include <cstdlib>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    int num1 = 7238;
-    const size_t bitCount = sizeof(num1)*8;
+    int num1 = atoi(argv[1]);
+    const size_t bitCount = sizeof(num1) * 8;
 
     // Counting the number of bits in the number num1
     size_t countOfOnes = 0;
     int bitmask = 1;
-    for(size_t i = 0;i<bitCount;++i)
+    for (size_t i = 0; i < bitCount; ++i)
     {
-        if(num1 & bitmask)
+        if (num1 & bitmask)
             countOfOnes++;
-        bitmask<<=1;
+        bitmask <<= 1;
     }
-    
-    std::cout<<countOfOnes<<"\n";
-    
+    std::cout << countOfOnes << "\n";
+
     return 0;
 }
