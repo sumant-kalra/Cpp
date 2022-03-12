feb5ffb9eed44c3ecf2f8e0abc6a10c359b8bf83
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-03-12 22:06:12 +0530

    chg: dev: Counting 1 in binary rep

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,84 +1,22 @@
-// Benchmarking
-
-#ifdef _MSC_VER_
-#define DEBUG_BREAK __debugbreak()
-#elif defined __APPLE__ || defined __unix__
-#define DEBUG_BREAK raise(SIGTRAP)
-#endif
-
-#include <signal.h>
 #include <iostream>
-#include <array>
-#include <memory>
-
-#include "Timer.h"
-#include "Entity.h"
-
+#include <bitset>
 
 int main(int argc, char* argv[])
 {
+    int num1 = 7238;
+    const size_t bitCount = sizeof(num1)*8;
+
+    // Counting the number of bits in the number num1
+    size_t countOfOnes = 0;
+    int bitmask = 1;
+    for(size_t i = 0;i<bitCount;++i)
     {
-        entity::UniquePtr_Entity ptrEn (new entity::Entity());
+        if(num1 & bitmask)
+            countOfOnes++;
+        bitmask<<=1;
     }
-
-
-#if 0
-    std::cout<<"Sum of integers:\n";
-    int sum = 0;
-    {
-        timer::Timer t;
-        for(int i =0;i<=100000;++i)
-            sum += i;
-    }
-    std::cout<<sum<<"\n";
-#endif 
-
-    // Array of 1000 shared pointers - using new keyword
-    std::array<std::shared_ptr<entity::Entity>,1000> arraySharedPtrsNew;
-
-    std::cout<<"1000 shared pointers allocation using 'new' keyword\n";
-    {
-        timer::Timer t;
-        for(auto& ptr:arraySharedPtrsNew)
-            ptr = std::shared_ptr<entity::Entity>(new entity::Entity());
-    }
-
-
-    // Array of 1000 shared pointers - using make_shared
-    std::array<std::shared_ptr<entity::Entity>,1000> arraySharedPtrsMakeShared;
-
-    std::cout<<"1000 shared pointers allocation using 'make_shared'\n";
-    {
-        timer::Timer t;
-        for(auto& ptr : arraySharedPtrsMakeShared) 
-            ptr = std::make_shared<entity::Entity>();
-    }
-
-
-    // Array of 1000 unique pointers - using new keyword
-    std::array<std::unique_ptr<entity::Entity>,1000> arrayUniquePtrsNew;
     
-    std::cout<<"1000 unique pointers allocation using 'new' keyword\n";
-    {
-        timer::Timer t;
-        for(auto& ptr : arrayUniquePtrsNew)
-            ptr = std::unique_ptr<entity::Entity>(new entity::Entity());
-    }
-
-    // Array of 1000 unique pointers - using make_unique keyword
-    std::array<std::unique_ptr<entity::Entity>,1000> arrayUniquePtrsMakeUnique;
-
-    std::cout<<"1000 unique pointers allocation using 'make_unique'\n";
-    {
-        timer::Timer t;
-        for(auto& ptr : arrayUniquePtrsMakeUnique)
-            ptr = std::make_unique<entity::Entity>();
-    }
-
-#if 0
-    // The code below will throw an exception if not run in debug mode
-    DEBUG_BREAK;
-#endif
-
+    std::cout<<countOfOnes<<"\n";
+    
     return 0;
 }
