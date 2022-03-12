927a5d126f324fab7893b0d7b3a403824815af78
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-03-12 22:45:06 +0530

    chg: dev: Improved-3

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-#include <cstdlib>
-
 int main(int argc, char *argv[])
 {
+    // Get the number from cmd line args
     int num1 = atoi(argv[1]);
-    const size_t bitCount = sizeof(num1) * 8;
 
-    // Counting the number of bits in the number num1
-    size_t countOfOnes = 0;
-    int bitmask = 1;
-    for (size_t i = 0; i < bitCount; ++i)
+    // Total number of bits used for the number
+    const size_t BIT_COUNT = sizeof(num1) * 8;
+
+    int countOfOnes = 0;
+    int bitmask = 1; // Binary number with same number of bits as the original number but the LSB as 1
+
+    // Iterating over all the bits in the number and checking every single bit if it is 1.
+    // If the bit is not 1 that it is 0, then the resulting number is also 0 otherwise
+    // it will be anything but 0.
+    for (size_t i = 0; i < BIT_COUNT; ++i)
     {
         if (num1 & bitmask)
             countOfOnes++;
         bitmask <<= 1;
     }
     std::cout << countOfOnes << "\n";
-
-    return 0;
 }
