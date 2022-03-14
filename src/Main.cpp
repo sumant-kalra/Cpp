54121e987fc4dbdfe737fddfd33455a81d0696c0
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-03-14 22:15:44 +0530

    chg: dev: 25-F2

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,38 +1,22 @@
 #include <iostream>
+#include <bitset>
 
 int main(int argc, char *argv[])
 {
     int num = atoi(argv[1]);
+    // The number after replacing the lowest bit that is 1 with 0
+    int num2 = num & (num - 1);
+    std::cout << "# The input number is: " << num << " (" << std::bitset<sizeof(num) * 8>(num) << ");\n";
+    std::cout << "# The new number after replacing the lowest bit that is 1: " << num2 << " (" << std::bitset<sizeof(num) * 8>(num2) << ")\n";
+
+    // Note that the above code can also be used to count the number of 1s present in the number
     int countOfOnes = 0;
-
-    // Approach 1
-    // Iterate over all the bits of the number input
-    // Check for each digit to be 1 or 0 by bitmasking with appropriate number
-#if 0
-    int bitmask = 1;
-    const size_t BIT_COUNT = sizeof(num) * 8;
-    for (size_t i = 0; i < BIT_COUNT; ++i)
+    while (num)
     {
-        if (num & bitmask)
-            ++countOfOnes;
-        bitmask <<= 1;
+        ++countOfOnes;
+        num &= (num - 1); // num = num & (num - 1) --> Equivalent to num = num ^ (num & ~(num-1))
     }
-    std::cout << "The count of 1s is: " << countOfOnes << "\n";
-#endif
-
-    // Approach 2
-    // Keep on isolating the lowest bit until we can and count it, while updating the original number with the 1 removed
-    int num2 = 0;
-    while (num) // The input is non-zero which means that it has atleast one bit which is 1
-    {
-        ++countOfOnes; // The number 'num' has one bit which is non-zero
-        // Since one bit which is non-zero is counted, we should remove a non zero bit from the number
-        // We will remove the lowest bit that is 1, from the number
-        num2 = num & ~(num - 1); // Isolate the lowest bit that is 1
-        num ^= num2;             // Remove the isolated bit from the original number
-        // Iterate until every 1 is removed
-    }
-    std::cout << "The count of 1s is: " << countOfOnes << "\n";
+    std::cout << "# The number of 1s present in the original number: " << countOfOnes << "\n";
 
     return 0;
 }
