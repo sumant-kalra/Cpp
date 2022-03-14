8ae86c81e26a49570b2383367b44795d1b5cecd4
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-03-14 22:53:53 +0530

    chg: dev: 25-Problem3

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,10 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-    int num = atoi(argv[1]);
-    // Right propagate the rightmost set bit in an integer valued number
-    int num2 = num | (num - 1);
-    std::cout << "# The input number is: " << num << " (" << std::bitset<sizeof(num) * 8>(num) << ");\n";
-    std::cout << "# The result of right propagation of the rightmost set bit is: " << num2 << " (" << std::bitset<sizeof(num) * 8>(num2) << ")\n";
+    unsigned int num = atoi(argv[1]);
+    unsigned int power = atoi(argv[2]);
+
+    // Compute an integer valued number modulo a power of 2
+    std::cout << num << "%2^(" << power << ") is: " << num % (1 << power) << "\n";
+
     return 0;
 }
