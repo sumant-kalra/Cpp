681bd1c53bb5bfa398e7d9aecfafaf448a7fd4cc
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-03-14 22:26:52 +0530

    chg: dev: 25-Problem2

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,19 +4,9 @@
 int main(int argc, char *argv[])
 {
     int num = atoi(argv[1]);
-    // The number after replacing the lowest bit that is 1 with 0
-    int num2 = num & (num - 1);
+    // Right propagate the rightmost set bit in an integer valued number
+    int num2 = num | (num - 1);
     std::cout << "# The input number is: " << num << " (" << std::bitset<sizeof(num) * 8>(num) << ");\n";
-    std::cout << "# The new number after replacing the lowest bit that is 1: " << num2 << " (" << std::bitset<sizeof(num) * 8>(num2) << ")\n";
-
-    // Note that the above code can also be used to count the number of 1s present in the number
-    int countOfOnes = 0;
-    while (num)
-    {
-        ++countOfOnes;
-        num &= (num - 1); // num = num & (num - 1) --> Equivalent to num = num ^ (num & ~(num-1))
-    }
-    std::cout << "# The number of 1s present in the original number: " << countOfOnes << "\n";
-
+    std::cout << "# The result of right propagation of the rightmost set bit is: " << num2 << " (" << std::bitset<sizeof(num) * 8>(num2) << ")\n";
     return 0;
 }
