d61be0f398e1383a76ce0fd6efbda50b3ba0d079
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-04-02 13:00:27 +0530

    chg: dev: Header only Libraries added

---
+++ ExternalLibs/utilities/Utilities.h
@@ -0,0 +1,28 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+// Logging system based on the preprocessor definition
+#if PR_RELEASE == 1
+#define LOG(x)
+#else
+#define LOG(x) std::cout << x << "\n";
+#endif
+
+#if 0
+#define MAIN    \
+    int main()  \
+    {           \
+        LOG(4); \
+    }
+#endif
+
+// Track memory allocation - FileName, LineNumber and Size of allocation in bytes
+#include <iostream>
+// The new function defined below does not throw exception but assigns a nullptr
+// x is the number of bytes to be allocated
+// new keyword comes from <new> which is also included in <iostream>
+#define NEW(x)                  \
+    new (std::nothrow) char(x); \
+    std::cout << "[Heap memory allocation] \n File name: " << __FILE__ << "; Line number: " << __LINE__ << "; Bytes allocated: " << x << "\n";
+
+#endif
