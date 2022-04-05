3c40173adb23c3e31b48207c6838219fcf954fba
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-04-05 22:44:25 +0530

    chg: dev: Random Numbers added

---
+++ src/Random.cpp
@@ -0,0 +1,49 @@
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <chrono>
+
+#include "Random.h"
+
+//------------------------------------------------------------//
+unsigned short int random_space::generatePIN_4_C()
+{
+    srand(time(NULL));
+    return (unsigned short int)((rand() % 9000) + 1000);
+}
+
+//------------------------------------------------------------//
+// distribution(generator)
+// Initialize/Seed the generator before using it
+unsigned short int random_space::generatePIN_4_cpp()
+{
+    std::uniform_int_distribution<int> distribution(1000, 9999);
+
+    unsigned int seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+    std::default_random_engine generator(seed);
+
+    return (unsigned short int)distribution(generator);
+}
+
+//------------------------------------------------------------//
+double random_space::generateRand_cpp(double min, double max)
+{
+    std::uniform_real_distribution<double> distribution(min, max);
+
+    unsigned int seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+    std::default_random_engine generator(seed);
+
+    return distribution(generator);
+}
+
+//------------------------------------------------------------//
+double random_space::generateRandCanonical()
+{
+    unsigned int seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+
+    // Generates a random floating point number in range [0,1)
+    return std::generate_canonical<double, 10>(generator);
+}
--- src/Random.h
+++ src/Random.h
@@ -1,5 +1,25 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+namespace random_space
+{
+    // Generate a 4 digit PIN using rand() of <cstdlib>
+    unsigned short int generatePIN_4_C();
+
+    // C++11
+    // Distribution bounds the output of the generator intialized with a seed
+
+    // 1. distribution(2. generator)
+    // 1.5 Initialize/Seed the generator with an unsigned value from any clock
+
+    // Generate a 4 digit PIN using <random> library of C++
+    unsigned short int generatePIN_4_cpp();
+
+    // Generate a real number randomly in the provided range
+    double generateRand_cpp(double min, double max);
+
+    // Generate a real number randomly in the range [0,1)
+    double generateRandCanonical();
+}
 
 #endif
