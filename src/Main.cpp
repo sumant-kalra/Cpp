13a85b5343861eee809046ac61426c8c511154bb
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-07-23 15:23:41 +0530

    chg: dev: Reset

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,36 +1,12 @@
 #define MAIN 1
+
 // if we want to track the memory allocation in this program then the new and delete operator overload definitions
 // must be provided in the main.cpp. Hence set the flag TRACK_MEMORY accordingly.
-#define TRACK_MEMORY 1
+#define TRACK_MEMORY 0
 #if MAIN
-
-#include <iostream>
-#include <memory>
-#include <memoryTrace/MemoryAllocationMetrics.h>
-
-struct Demo
-{
-    int x = 0;
-    float y = 0;
-    int z = 0;
-};
 
 int main(int argc, char const *argv[])
 {
-    Demo *ptr_obj = new Demo;
-    metrics::MemoryAllocationMetrics::printMemoryUsage();
-
-    delete ptr_obj;
-    metrics::MemoryAllocationMetrics::printMemoryUsage();
-
-    {
-        std::unique_ptr<Demo> ptrDemo = std::make_unique<Demo>();
-        metrics::MemoryAllocationMetrics::printMemoryUsage();
-    }
-    metrics::MemoryAllocationMetrics::printMemoryUsage();
-
-    std::string name = "This is my Memory Tracker Program";
-    metrics::MemoryAllocationMetrics::printMemoryUsage();
 
     return 0;
 }
@@ -38,7 +14,7 @@ int main(int argc, char const *argv[])
 #if TRACK_MEMORY
 // Definition of new and delete is deliberately kept here to avoid linking with the original new and delete definitions
 // The below definitions should be activated when
-
+#include "memoryTrace/MemoryAllocationMetrics.h"
 void *operator new(size_t sizeInBytes)
 {
     metrics::MemoryAllocationMetrics::addMemory(sizeInBytes);
