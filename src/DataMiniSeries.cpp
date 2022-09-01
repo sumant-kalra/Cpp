518d026a67a4d435efe0d8b50a7d528603f4a4a1
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-09-01 07:23:31 +0530

    chg: dev: any

--- src/DataMiniSeries.cpp
+++ src/DataMiniSeries.cpp
@@ -1,13 +1,85 @@
 #define DATA_MINI_SERIES_MAIN 1
+#define READ_FILE 0
 #define STD_OPTIONAL 0
-#define STD_VARIANT 1
-#define STD_ANY 0
+#define STD_VARIANT 0
+#define STD_ANY 1
 
 #if DATA_MINI_SERIES_MAIN
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
+
+#if READ_FILE
+static std::string readFromFile(std::ifstream &file)
+{
+    std::stringstream ss;
+    std::string tempString;
+
+    // For the other cases when we are not checking the state all the time, use fstream's exception mask instead of the code below
+    while (getline(file, tempString))
+    {
+        ss << tempString << '\n';
+    }
+    if (!file.eof())
+    {
+        throw std::invalid_argument("Failed to read the file");
+    }
+
+    return ss.str();
+}
+
+static std::string openAndReadFromFile(const std::string &fileName)
+{
+    std::ifstream file(fileName);
+
+    if (!file.is_open())
+        throw std::invalid_argument("Failed to open the file");
+
+    std::string fileContents;
+    try
+    {
+        fileContents = readFromFile(file);
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown Error!\n";
+    }
+
+    file.close();
+    return fileContents;
+}
+
+int main(int argc, char const *argv[])
+{
+    try
+    {
+        std::cout << openAndReadFromFile("TestFile.txt");
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown Error!\n";
+    }
+
+    return 0;
+}
+#endif // READ_FILE
 
 #if STD_OPTIONAL
 #include <optional>
@@ -146,6 +218,26 @@ int main(int argc, char const *argv[])
 #endif // STD_VARIANT
 
 #if STD_ANY
+#include <any>
+int main(int argc, char const *argv[])
+{
+    std::any variable;
+    variable = 6.0f;
+    variable = 5;
+
+    auto var1 = std::any_cast<int &>(variable);
+    std::cout << var1 << "\n";
+
+    variable = 7.0;
+    variable = "This is a test";
+
+    // auto out = std::any_cast<std::string &>(variable); // Fails as the type of the variable is const char*
+    auto out = std::any_cast<const char *>(variable);
+    std::cout << out << "\n";
+
+    return 0;
+}
+
 #endif // STD_ANY
 
 #endif // DATA_MINI_SERIES_MAIN
